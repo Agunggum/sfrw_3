06c0cefb429d99a5add8df5bdb5b4d5f5f2f12f7
Agunggum <agunggum@gmail.com>
2025-12-08 12:39:30 +0700

    update header

--- mvc/view/header.view.php
+++ mvc/view/header.view.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php echo WEBTITLETOP; ?></title>
     <meta name="description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
-    <link rel="icon" href="<?php echo BASEURL; ?>bootstrap/theme/logo-sfrw.ico" sizes="any" >
-    <link rel="icon" href="<?php echo BASEURL; ?>bootstrap/theme/logo-sfrw.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="<?php echo BASEURL; ?>bootstrap/theme/logo-sfrw.png">
+    <link rel="icon" href="bootstrap/theme/logo-sfrw.ico" sizes="any" >
+    <link rel="icon" href="bootstrap/theme/logo-sfrw.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="bootstrap/theme/logo-sfrw.png">
     <meta property="og:title" content="<?php echo WEBTITLETOP; ?>>" />
-    <meta property="og:image" content="<?php echo BASEURL; ?>bootstrap/theme/globe-network.png" />
+    <meta property="og:image" content="bootstrap/theme/globe-network.png" />
     <meta property="og:url" content="<?php echo  BASEURL; ?>" />
     <meta property="og:description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
     <meta property="og:site_name" content="<?php echo WEBTITLE; ?>" />
