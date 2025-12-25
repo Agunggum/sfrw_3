86a66893b17c93a474d3935f353122e2f5b9efa1
Agunggum <agunggum@gmail.com>
2025-12-25 19:46:58 +0700

    update view pagenotfound

--- mvc/view/pagenotfound.view.php
+++ mvc/view/pagenotfound.view.php
@@ -1,9 +1,1 @@
-<?php if ( ! defined('APPPATH')) exit('No direct script access allowed');
-$dates = gmdate("Y-m-d H:i:s", time()+60*60*7);
-$_SESSION['XfTVKuhxT3LUAbp5C8z37lHdj'] = "E-ROUTE-404";
-$_SESSION['zyA2QF2M25e3TyVmi2w99n2tB'] = "Route not found: ".ROUTE;
-$_SESSION['6vhow83GCbV6jdXTMEgAJdqEN'] = "0".", "."Route not found: ".ROUTE.", , ".$_SERVER['REQUEST_URI'].", ".$dates;
-if($_SESSION['XfTVKuhxT3LUAbp5C8z37lHdj']=='E-ROUTE-404'){
-echo "<script>setTimeout(function () { document.location='".$_SERVER['REQUEST_URI']."'; }, 2);</script>";
-}
-?>
+<?php if ( ! defined('APPPATH')) exit('No direct script access allowed'); require_once BASEPATH."error/404handler".EXT; ?>
