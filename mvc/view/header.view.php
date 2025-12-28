219ddefcd60e1e6cd8b67ccc30d6a4dacd79e99b
Agunggum <agunggum@gmail.com>
2025-12-28 10:54:51 +0700

    update header

--- mvc/view/header.view.php
+++ mvc/view/header.view.php
@@ -10,7 +10,7 @@
     <link rel="icon" href="bootstrap/theme/logo-sfrw.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="bootstrap/theme/logo-sfrw.png">
     <meta property="og:title" content="<?php echo WEBTITLETOP; ?>>" />
-    <meta property="og:image" content="bootstrap/theme/globe-network.png" />
+    <meta property="og:image" content="bootstrap/theme/logo-sfrw.png" />
     <meta property="og:url" content="<?php echo BASEURL; ?>" />
     <meta property="og:description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
     <meta property="og:site_name" content="<?php echo WEBTITLE; ?>" />
