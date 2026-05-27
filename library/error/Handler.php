b954b08ed9854ce24a169871c4833fd997ee0201
Agunggum <agunggum@gmail.com>
2026-05-27 21:20:28 +0700

    Update framework

--- library/error/Handler.php
+++ library/error/Handler.php
@@ -27,7 +27,7 @@ if (DEBUG == 'true'):
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>S-FRW Error: <?php echo htmlspecialchars($error['errstr']); ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo asset('bootstrap/theme/css/bootstrap.min.css?v=5.3.8'); ?>" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.ico'); ?>" sizes="any" >
     <link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.svg'); ?>" type="image/svg+xml">
