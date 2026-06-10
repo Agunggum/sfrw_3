61abf308277d0436c1ecdc7fd775b84362e867cc
Agunggum <agunggum@gmail.com>
2026-06-10 16:01:57 +0700

    Update framework

--- mvc/view/users/daftar.view.php
+++ mvc/view/users/daftar.view.php
@@ -14,7 +14,7 @@ $key = encrypt(date('YmdHi')); ?>
     }
 </style>
 <section>
-    <table id="datatable-users" class="datatable-help table table-striped">
+    <table id="datatable-users" class="table table-striped">
         <thead>
             <tr>
                 <th>No</th>
