9ec96a018c59e041e9a443ec20bed0a2e3e3d720
Agunggum <agunggum@gmail.com>
2026-06-30 18:36:05 +0700

    Update framework

--- vendor/sfrw/framework/src/muhammad/contracs/container/Container.php
+++ vendor/sfrw/framework/src/muhammad/contracs/container/Container.php
@@ -711,6 +711,25 @@ function dateandtime_indo($date) {
 	return $tgl." ".get_month_indo($bln)." ".$thn.", ".$time;
 }
 
+function daydateandtime_indo($date) {
+    list($ltgl,$time)=explode(' ',$date);
+    list($thn,$bln,$tgl)=explode('-',$ltgl);
+    $hari = date ("D",  strtotime($ltgl));
+
+    switch($hari){
+		case 'Sun': $hari_ini = "Minggu"; break;
+		case 'Mon': $hari_ini = "Senin"; break;
+		case 'Tue': $hari_ini = "Selasa"; break;
+		case 'Wed': $hari_ini = "Rabu"; break;
+		case 'Thu': $hari_ini = "Kamis"; break;
+		case 'Fri': $hari_ini = "Jumat"; break;
+		case 'Sat': $hari_ini = "Sabtu"; break;
+        default: $hari_ini = ""; break;
+	}
+
+	return $hari_ini.", ".$tgl." ".get_month_indo($bln)." ".$thn." ".$time;
+}
+
 function daydateandtime_en($date) {
     list($ltgl,$time)=explode(' ',$date);
     list($thn,$bln,$tgl)=explode('-',$ltgl);
