3f00b491b90b6a285fa702b8a1f4bdfcca941974
Agunggum <agunggum@gmail.com>
2026-07-10 08:09:16 +0700

    Update framework

--- app/Services/Pengirimmail.php
+++ app/Services/Pengirimmail.php
@@ -7,9 +7,9 @@ class Pengirimmail {
 
         ob_start();
         include tampilan('mailer', [
-            $data['subject'] = $subject,
+            $data['subject'] = $judul,
             $data['title'] = WEBTITLETOP,
-            $data['content'] = $content,
+            $data['content'] = $isiPesan,
             $data['tahun'] = $tahun_sekarang,
         ]);
         $html_email = ob_get_clean();
@@ -26,10 +26,10 @@ class Pengirimmail {
                 $mail->Username = MAILUSER;
                 $mail->Password = MAILPASS;
                 $mail->SetFrom(MAILSENT, MAILTITLE);
-                $mail->AddAddress($email_tujuan, $nama_user);
+                $mail->AddAddress($emailPenerima, $namaPenerima);
                 // --- KONTEN EMAIL ---
                 $mail->isHTML(true); // Set format email ke HTML
-                $mail->Subject = $subject." - ".MAILTITLE;
+                $mail->Subject = $judul." - ".MAILTITLE;
                 $mail->Body    = $html_email;
                 $mail->Send();
                 $message = "<br><br>Mail sent successfully";
@@ -42,7 +42,7 @@ class Pengirimmail {
             // Kirim email dalam format HTML
             $headersMail  = "From: ".MAILTITLE." <".MAILSENT.">\r\n";
             $headersMail .= "Content-type: text/html\r\n";
-            mail($email_tujuan, $subjectMail, $html_email, $headersMail);
+            mail($emailPenerima, $subjectMail, $html_email, $headersMail);
             $message = "<br><br>Mail sent successfully";
         }
 
