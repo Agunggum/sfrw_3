9ea692a33729fe8d90d66ef220d7b57d319ef7d4
Agunggum <agunggum@gmail.com>
2026-07-24 15:08:33 +0700

    Update framework

--- mvc/view/index.view.php
+++ mvc/view/index.view.php
@@ -22,19 +22,25 @@
                         
                         <div class="dropdown" id="language-dropdown-container">
                             <button class="btn btn-link nav-link dropdown-toggle d-flex align-items-center" id="bd-language" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle language">
-                                <i id="current-flag-icon" class="fi fi-id language-icon-active me-2"></i>
+                                <div class="d-inline-flex align-items-center justify-content-center bg-white text-white rounded-circle overflow-hidden overflow-ellipsis me-1" style="width: 20px; height: 20px;">
+                                <i id="current-flag-icon" class="fi fi-id language-icon-active"></i>
+                                </div>
                             </button>
                             <ul class="dropdown-menu dropdown-menu-end shadow-sm" aria-labelledby="bd-language-text">
                                 <li>
                                     <button type="button" class="dropdown-item d-flex align-items-center lang-item" data-lang="id" data-bs-language-value="id-ID" aria-pressed="false" onclick="changeLanguage('id')">
-                                        <i class="fi fi-id me-2 opacity-50 language-icon"></i>
+                                        <div class="d-inline-flex align-items-center justify-content-center bg-white text-white rounded-circle overflow-hidden overflow-ellipsis me-2" style="width: 20px; height: 20px;">
+                                            <i class="fi fi-id opacity-75 language-icon"></i>
+                                        </div>
                                         Indonesia
                                         <i class="bi bi-check2 ms-auto d-none check-icon"></i>
                                     </button>
                                 </li>
                                 <li>
                                     <button type="button" class="dropdown-item d-flex align-items-center lang-item" data-lang="en" data-bs-language-value="en-US" aria-pressed="false" onclick="changeLanguage('en')">
-                                        <i class="fi fi-us me-2 opacity-50 language-icon"></i>
+                                        <div class="d-inline-flex align-items-center justify-content-center bg-white text-white rounded-circle overflow-hidden overflow-ellipsis me-2" style="width: 20px; height: 20px;">
+                                            <i class="fi fi-us opacity-75 language-icon"></i>
+                                        </div>
                                         English
                                         <i class="bi bi-check2 ms-auto d-none check-icon"></i>
                                     </button>
