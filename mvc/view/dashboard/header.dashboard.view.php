9efe41ce65f1450881778037de177012d14cfecf
Agunggum <agunggum@gmail.com>
2026-07-31 14:05:21 +0700

    Update framework

--- mvc/view/dashboard/header.dashboard.view.php
+++ mvc/view/dashboard/header.dashboard.view.php
@@ -11,47 +11,47 @@ if (defined('IS_AJAX') && IS_AJAX) {
 <html lang="en" data-bs-theme="auto" data-spa-layout="dashboard">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $data['title']; ?></title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo $data['title']; ?></title>
 
-    <link rel="manifest" href="<?php echo asset('manifest.json'); ?>">
+<link rel="manifest" href="<?php echo asset('manifest.json'); ?>">
   
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="theme-color" content="#212529">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <link rel="apple-touch-icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="theme-color" content="#212529">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<link rel="apple-touch-icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>">
 
-    <meta name="description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
-    <link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.ico'); ?>" sizes="any">
-    <link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.svg'); ?>" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>">
-    <meta property="og:title" content="<?php echo $data['title']; ?>" />
-    <meta property="og:image" content="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>" />
-    <meta property="og:url" content="<?php echo BASEURL; ?>" />
-    <meta property="og:description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
-    <meta property="og:site_name" content="<?php echo WEBTITLE; ?>" />
-    <style>
-        @import "<?php echo asset('bootstrap/theme/css/bootstrap.css?v=5.3.8'); ?>";
-        @import "<?php echo asset('bootstrap/theme/css/bootstrap.min.css?v=5.3.8'); ?>";
-        @import "<?php echo asset('bootstrap/theme/css/datatables-bootstrap-5.css?v=0.1'); ?>";
-        <?php /*@import "<?php echo asset('bootstrap/theme/fontawesome/css/all.css'); ?>*/ ?>
-        @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css");
-        @import url("https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.3.2/css/flag-icons.min.css");
-        @import "<?php echo asset('bootstrap/theme/css/sfrw_dashboard.css?v=0.7'); ?>";
-    </style>
-    <script src="<?php echo asset('bootstrap/theme/js/jquery-3.7.1.js'); ?>"></script>
-    <?php /*<script src="<?php echo asset('bootstrap/theme/fontawesome/js/all.js'); ?>" defer></script>*/ ?>
-    <script src="<?php echo asset('bootstrap/theme/js/bootstrap.min.js'); ?>" defer></script>
-    <script src="<?php echo asset('bootstrap/theme/js/bootstrap.bundle.min.js'); ?>" defer></script>
-    <script src="<?php echo asset('bootstrap/theme/js/datatables.js'); ?>" defer></script>
-    <script src="<?php echo asset('bootstrap/theme/js/dataTables.bootstrap5.js'); ?>" defer></script>
-    <script>
-        // Global variables for JS
-        window.BASEURL = '<?php echo BASEURL; ?>';
-        window.BASEURL_PATH = '<?php echo parse_url(BASEURL, PHP_URL_PATH); ?>';
-    </script>
-    <script type="module" src="<?php echo asset('bootstrap/theme/js/app.js?v=0.2'); ?>"></script>
+<meta name="description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
+<link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.ico'); ?>" sizes="any">
+<link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.svg'); ?>" type="image/svg+xml">
+<link rel="apple-touch-icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>">
+<meta property="og:title" content="<?php echo $data['title']; ?>" />
+<meta property="og:image" content="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>" />
+<meta property="og:url" content="<?php echo BASEURL; ?>" />
+<meta property="og:description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
+<meta property="og:site_name" content="<?php echo WEBTITLE; ?>" />
+<style>
+@import "<?php echo asset('bootstrap/theme/css/bootstrap.css?v=5.3.8'); ?>";
+@import "<?php echo asset('bootstrap/theme/css/bootstrap.min.css?v=5.3.8'); ?>";
+@import "<?php echo asset('bootstrap/theme/css/datatables-bootstrap-5.css?v=0.1'); ?>";
+<?php /*@import "<?php echo asset('bootstrap/theme/fontawesome/css/all.css'); ?>*/ ?>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css");
+@import url("https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.3.2/css/flag-icons.min.css");
+@import "<?php echo asset('bootstrap/theme/css/sfrw_dashboard.css?v=0.7'); ?>";
+</style>
+<script src="<?php echo asset('bootstrap/theme/js/jquery-3.7.1.js'); ?>"></script>
+<?php /*<script src="<?php echo asset('bootstrap/theme/fontawesome/js/all.js'); ?>" defer></script>*/ ?>
+<script src="<?php echo asset('bootstrap/theme/js/bootstrap.min.js'); ?>" defer></script>
+<script src="<?php echo asset('bootstrap/theme/js/bootstrap.bundle.min.js'); ?>" defer></script>
+<script src="<?php echo asset('bootstrap/theme/js/datatables.js'); ?>" defer></script>
+<script src="<?php echo asset('bootstrap/theme/js/dataTables.bootstrap5.js'); ?>" defer></script>
+<script>
+// Global variables for JS
+window.BASEURL = '<?php echo BASEURL; ?>';
+window.BASEURL_PATH = '<?php echo parse_url(BASEURL, PHP_URL_PATH); ?>';
+</script>
+<script type="module" src="<?php echo asset('bootstrap/theme/js/app.js?v=0.2'); ?>"></script>
     
 </head>
 <body>
--- mvc/view/header.view.php
+++ mvc/view/header.view.php
@@ -9,48 +9,48 @@ if (defined('IS_AJAX') && IS_AJAX) {
 <!doctype html>
 <html class="no-js" lang="en" data-bs-theme="light" data-spa-layout="public">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php echo $data['title']; ?></title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title><?php echo $data['title']; ?></title>
 
-    <link rel="manifest" href="<?php echo asset('manifest.json'); ?>">
+<link rel="manifest" href="<?php echo asset('manifest.json'); ?>">
   
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="theme-color" content="#212529">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <link rel="apple-touch-icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="theme-color" content="#212529">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<link rel="apple-touch-icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>">
 
-    <meta name="description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
-    <link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.ico'); ?>" sizes="any" >
-    <link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.svg'); ?>" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>">
-    <meta property="og:title" content="<?php echo $data['title']; ?>" />
-    <meta property="og:image" content="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>" />
-    <meta property="og:url" content="<?php echo BASEURL; ?>" />
-    <meta property="og:description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
-    <meta property="og:site_name" content="<?php echo WEBTITLE; ?>" />
-    <style>
-        @import "<?php echo asset('bootstrap/theme/css/bootstrap.css?v=5.3.8'); ?>";
-        @import "<?php echo asset('bootstrap/theme/css/bootstrap.min.css?v=5.3.8'); ?>";
-        @import "<?php echo asset('bootstrap/theme/css/datatables-bootstrap-5.css?v=0.1'); ?>";
-        <?php /*@import "<?php echo asset('bootstrap/theme/fontawesome/css/all.css'); ?>*/ ?>
-        @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css");
-        @import url("https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.3.2/css/flag-icons.min.css");
-        @import "<?php echo asset('bootstrap/theme/css/sfrw.css?v=0.4'); ?>";
-    </style>
-    <script src="<?php echo asset('bootstrap/theme/js/jquery-3.7.1.js'); ?>"></script>
-    <?php /*<script src="<?php echo asset('bootstrap/theme/fontawesome/js/all.js'); ?>" defer></script>*/ ?>
-    <script src="<?php echo asset('bootstrap/theme/js/bootstrap.min.js'); ?>" defer></script>
-    <script src="<?php echo asset('bootstrap/theme/js/bootstrap.bundle.min.js'); ?>" defer></script>
-    <script src="<?php echo asset('bootstrap/theme/js/datatables.js'); ?>" defer></script>
-    <script src="<?php echo asset('bootstrap/theme/js/dataTables.bootstrap5.js'); ?>" defer></script>
-    <script>
-        // Global variables for JS
-        window.BASEURL = '<?php echo BASEURL; ?>';
-        window.BASEURL_PATH = '<?php echo parse_url(BASEURL, PHP_URL_PATH); ?>';
-    </script>
-    <script type="module" src="<?php echo asset('bootstrap/theme/js/app.js?v=0.3'); ?>"></script>
+<meta name="description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
+<link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.ico'); ?>" sizes="any" >
+<link rel="icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.svg'); ?>" type="image/svg+xml">
+<link rel="apple-touch-icon" href="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>">
+<meta property="og:title" content="<?php echo $data['title']; ?>" />
+<meta property="og:image" content="<?php echo asset('bootstrap/theme/logo-sfrw.png'); ?>" />
+<meta property="og:url" content="<?php echo BASEURL; ?>" />
+<meta property="og:description" content="<?php echo WEBTITLE; ?> <?php echo VERSIONFRMAEWORK; ?>" />
+<meta property="og:site_name" content="<?php echo WEBTITLE; ?>" />
+<style>
+@import "<?php echo asset('bootstrap/theme/css/bootstrap.css?v=5.3.8'); ?>";
+@import "<?php echo asset('bootstrap/theme/css/bootstrap.min.css?v=5.3.8'); ?>";
+@import "<?php echo asset('bootstrap/theme/css/datatables-bootstrap-5.css?v=0.1'); ?>";
+<?php /*@import "<?php echo asset('bootstrap/theme/fontawesome/css/all.css'); ?>*/ ?>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css");
+@import url("https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.3.2/css/flag-icons.min.css");
+@import "<?php echo asset('bootstrap/theme/css/sfrw.css?v=0.4'); ?>";
+</style>
+<script src="<?php echo asset('bootstrap/theme/js/jquery-3.7.1.js'); ?>"></script>
+<?php /*<script src="<?php echo asset('bootstrap/theme/fontawesome/js/all.js'); ?>" defer></script>*/ ?>
+<script src="<?php echo asset('bootstrap/theme/js/bootstrap.min.js'); ?>" defer></script>
+<script src="<?php echo asset('bootstrap/theme/js/bootstrap.bundle.min.js'); ?>" defer></script>
+<script src="<?php echo asset('bootstrap/theme/js/datatables.js'); ?>" defer></script>
+<script src="<?php echo asset('bootstrap/theme/js/dataTables.bootstrap5.js'); ?>" defer></script>
+<script>
+// Global variables for JS
+window.BASEURL = '<?php echo BASEURL; ?>';
+window.BASEURL_PATH = '<?php echo parse_url(BASEURL, PHP_URL_PATH); ?>';
+</script>
+<script type="module" src="<?php echo asset('bootstrap/theme/js/app.js?v=0.3'); ?>"></script>
     
 </head>
 <body>
